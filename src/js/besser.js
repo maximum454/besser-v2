0c45c9347c060d0db9e5ef6356f70f0616596e1c
maxim <maximum454@gmail.com>
2020-09-03 23:38:18 +0600

    index

--- src/js/besser.js
+++ src/js/besser.js
@@ -1,13 +1,38 @@
 @@include('partials/jquery.min.js')
 @@include('partials/webp.js')
 @@include('partials/slick.js')
+@@include('./partials/TweenLite.min.js')
+@@include('./partials/TimelineMax.min.js')
+@@include('./partials/EasePack.min.js')
+@@include('./partials/CSSPlugin.min.js')
 
 $(function () {
-    $('.js-slider-main').slick({
+    var sliderMain = $('.js-slider-main').slick({
         dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
+        adaptiveHeight: false
+    });
+
+    sliderMain.on('afterChange', function(event, slick, current){
+        console.log(current);
+        sliderRubric = document.getElementsByClassName('slider-main__rubric');
+        sliderName = document.getElementsByClassName('slider-main__name');
+        sliderPrice = document.getElementsByClassName('slider-main__price');
+        tl = new TimelineMax();
+        tl
+            .fromTo(sliderRubric, .6, {left: "0",top: "-30px"}, {left: '0',top: '0',ease:Linear.easeNone},'group1')
+            .fromTo(sliderName, .6, {left: "100%",top: "0"}, {left: '0',top: '0',ease:Linear.easeNone},'group1')
+            .fromTo(sliderPrice, .6, {opacity: '0'}, {opacity: '1',ease:Linear.easeNone})
+    });
+
+    $('.js-slider-partners').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
         adaptiveHeight: false
     });
 })
